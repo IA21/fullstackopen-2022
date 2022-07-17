fc08cd10db37349a0ca2e08f79679e3caccbf98f
IA21 <18244928+IA21@users.noreply.github.com>
2022-07-17 15:34:00 +0500

    2.5

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,29 +1,4 @@
-const Header = ({ course }) => <h2>{course.name}</h2>
-
-const Total = ({ parts }) => {
-   return (
-      <p><strong>
-         total of {parts.reduce((sum, part) => sum + part.exercises, 0)} exercises
-      </strong></p>
-   )
-}
-
-const Part = ({ part }) => <p>{part.name} {part.exercises}</p>
-
-const Content = ({ parts }) =>
-   <>
-      {parts.map((part) => <Part key={part.id} part={part} />)}
-   </>
-
-const Course = ({ course }) => {
-   return (
-      <>
-         <Header course={course} />
-         <Content parts={course.parts} />
-         <Total parts={course.parts} />
-      </>
-   )
-}
+import Course from './Course'
 
 const App = () => {
    const courses = [
