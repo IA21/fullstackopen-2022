8db8635f3a05b13362ff9b69a878a6b86a3f74d6
IA21 <18244928+IA21@users.noreply.github.com>
2022-07-18 13:34:22 +0500

    2.9

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,10 +2,18 @@ import { useState } from 'react'
 
 const App = () => {
    const [persons, setPersons] = useState([
-      { name: 'Arto Hellas', number: '040-1234567' }
+      { name: 'Arto Hellas', number: '040-123456', id: 1 },
+      { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+      { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+      { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
    ])
+   const [filterName, setFilterName] = useState('')
    const [newName, setNewName] = useState('')
    const [newNumber, setNewNumber] = useState('')
+
+   const handleFilterChange = (e) => {
+      setFilterName(e.target.value)
+   }
 
    const handleFormSubmit = (e) => {
       e.preventDefault()
@@ -23,13 +31,18 @@ const App = () => {
    return (
       <div>
          <h2>Phonebook</h2>
+         <label>filter shown with <input value={filterName} onChange={handleFilterChange} /></label>
+         <h1>add a new</h1>
          <form onSubmit={handleFormSubmit}>
-            <div>name: <input value={newName} onChange={(e) => setNewName(e.target.value)} autoFocus /></div>
+            <div>name: <input value={newName} onChange={(e) => setNewName(e.target.value)} /></div>
             <div>number: <input value={newNumber} onChange={(e) => setNewNumber(e.target.value)} /></div>
             <div><button type="submit">add</button></div>
          </form>
          <h2>Numbers</h2>
-         {persons.map((person) => <p key={person.name}>{person.name} {person.number}</p>)}
+         {persons.map((person) => {
+            if (person.name.toLowerCase().includes(filterName.trim().toLowerCase()))
+               return <div key={person.name}>{person.name} {person.number}</div>
+         })}
       </div>
    )
 }
