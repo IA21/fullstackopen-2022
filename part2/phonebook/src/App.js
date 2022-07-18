2a12dbbc3ed9fa053d3b955136f8d9cf0dd4d2da
IA21 <18244928+IA21@users.noreply.github.com>
2022-07-18 13:18:58 +0500

    2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,9 +2,10 @@ import { useState } from 'react'
 
 const App = () => {
    const [persons, setPersons] = useState([
-      { name: 'Arto Hellas' }
+      { name: 'Arto Hellas', number: '040-1234567' }
    ])
    const [newName, setNewName] = useState('')
+   const [newNumber, setNewNumber] = useState('')
 
    const handleFormSubmit = (e) => {
       e.preventDefault()
@@ -14,23 +15,21 @@ const App = () => {
          return;
       }
 
-      setPersons(persons.concat({ name: newName }))
+      setPersons(persons.concat({ name: newName, number: newNumber }))
       setNewName('')
+      setNewNumber('')
    }
 
    return (
       <div>
          <h2>Phonebook</h2>
          <form onSubmit={handleFormSubmit}>
-            <div>
-               name: <input value={newName} onChange={(e) => setNewName(e.target.value)} autoFocus />
-            </div>
-            <div>
-               <button type="submit">add</button>
-            </div>
+            <div>name: <input value={newName} onChange={(e) => setNewName(e.target.value)} autoFocus /></div>
+            <div>number: <input value={newNumber} onChange={(e) => setNewNumber(e.target.value)} /></div>
+            <div><button type="submit">add</button></div>
          </form>
          <h2>Numbers</h2>
-         {persons.map((person) => <p key={person.name}>{person.name}</p>)}
+         {persons.map((person) => <p key={person.name}>{person.name} {person.number}</p>)}
       </div>
    )
 }
