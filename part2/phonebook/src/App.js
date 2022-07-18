6f5806026085bc03303c0e99d6ad53478dbfdfdf
IA21 <18244928+IA21@users.noreply.github.com>
2022-07-18 13:06:09 +0500

    2.6

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+
+const App = () => {
+   const [persons, setPersons] = useState([
+      { name: 'Arto Hellas' }
+   ])
+   const [newName, setNewName] = useState('')
+
+   const handleFormSubmit = (e) => {
+      e.preventDefault()
+      setPersons(persons.concat({ name: newName }))
+      setNewName('')
+   }
+
+   return (
+      <div>
+         <h2>Phonebook</h2>
+         <form onSubmit={handleFormSubmit}>
+            <div>
+               name: <input value={newName} onChange={(e) => setNewName(e.target.value)} autoFocus />
+            </div>
+            <div>
+               <button type="submit">add</button>
+            </div>
+         </form>
+         <h2>Numbers</h2>
+         {persons.map((person) => <p key={person.name}>{person.name}</p>)}
+      </div>
+   )
+}
+
+export default App
