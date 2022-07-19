603d2f86298ae51cae42a0ab75f375030b416b41
IA21 <18244928+IA21@users.noreply.github.com>
2022-07-19 15:14:21 +0500

    2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,54 +1,56 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
 const App = () => {
-   const [persons, setPersons] = useState([
-      { name: 'Arto Hellas', number: '040-123456', id: 1 },
-      { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-      { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-      { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-   ])
-   const [filterName, setFilterName] = useState('')
-   const [newName, setNewName] = useState('')
-   const [newNumber, setNewNumber] = useState('')
+    const [persons, setPersons] = useState([])
+    const [filterName, setFilterName] = useState('')
+    const [newName, setNewName] = useState('')
+    const [newNumber, setNewNumber] = useState('')
 
-   const handleFilterChange = (e) => {
-      setFilterName(e.target.value)
-   }
+    useEffect(() => {
+        axios.get('http://localhost:3001/persons').then(resp => {
+            setPersons(resp.data)
+        })
+    }, [])
 
-   const handleFormSubmit = (e) => {
-      e.preventDefault()
+    const handleFilterChange = (e) => {
+        setFilterName(e.target.value)
+    }
 
-      if (persons.filter(person => person.name.toLowerCase() === newName.trim().toLowerCase()).length > 0) {
-         alert(`${newName} is already added to phonebook`)
-         return;
-      }
+    const handleFormSubmit = (e) => {
+        e.preventDefault()
 
-      setPersons(persons.concat({ name: newName, number: newNumber }))
-      setNewName('')
-      setNewNumber('')
-   }
+        if (persons.filter(person => person.name.toLowerCase() === newName.trim().toLowerCase()).length > 0) {
+            alert(`${newName} is already added to phonebook`)
+            return;
+        }
 
-   return (
-      <div>
-         <h2>Phonebook</h2>
-         <Filter filterName={filterName} handleFilterChange={handleFilterChange} />
+        setPersons(persons.concat({ name: newName, number: newNumber }))
+        setNewName('')
+        setNewNumber('')
+    }
 
-         <h2>add a new</h2>
-         <PersonForm
-            handleFormSubmit={handleFormSubmit}
-            newName={newName}
-            nameChangeHandler={(e) => setNewName(e.target.value)}
-            newNumber={newNumber}
-            numberChangeHandler={(e) => setNewNumber(e.target.value)}
-         />
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <Filter filterName={filterName} handleFilterChange={handleFilterChange} />
 
-         <h2>Numbers</h2>
-         <Persons persons={persons} filterName={filterName} />
-      </div>
-   )
+            <h2>add a new</h2>
+            <PersonForm
+                handleFormSubmit={handleFormSubmit}
+                newName={newName}
+                nameChangeHandler={(e) => setNewName(e.target.value)}
+                newNumber={newNumber}
+                numberChangeHandler={(e) => setNewNumber(e.target.value)}
+            />
+
+            <h2>Numbers</h2>
+            <Persons persons={persons} filterName={filterName} />
+        </div>
+    )
 }
 
 export default App
