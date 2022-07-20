17b9000af6b96eb844c6c5bf81d8efe771d0943a
IA21 <18244928+IA21@users.noreply.github.com>
2022-07-20 11:54:02 +0500

    2.13

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,7 +1,22 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const CountryList = ({ countries, countryFilter, countriesLoaded }) => {
+const CountryDetails = ({ country }) => {
+    return (
+        <>
+            <h1>{country.name.common}</h1>
+            <div>capital {country.capital}</div>
+            <div>area {country.area}</div>
+            <h3>languages:</h3>
+            <ul>
+                {Object.values(country.languages).map((lang, i) => <li key={i}>{lang}</li>)}
+            </ul>
+            <img src={country.flags.png} width={180} alt='flag' />
+        </>
+    )
+}
+
+const CountryList = ({ countries, countryFilter, countriesLoaded, countryDetailsShown, setCountryDetailsShown }) => {
     let filtered_countries = countries.filter(country => country.name.common.toLowerCase().includes(countryFilter.toLowerCase()));
     // console.log(filtered_countries)
 
@@ -14,12 +29,21 @@ const CountryList = ({ countries, countryFilter, countriesLoaded }) => {
             <div>Too many matches, specify another filter ({filtered_countries.length})</div>
         )
     } else if (filtered_countries.length > 1) {
+        const handleShowCountry = (i) => {
+            let old_state = [...countryDetailsShown];
+            old_state[i] = !old_state[i];
+            setCountryDetailsShown(old_state);
+        }
+
         return (
             <>
                 {
                     filtered_countries.map((country, index) => {
                         return (
-                            <div key={index}>{country.name.common}</div>
+                            <div key={index}>
+                                {country.name.common} <button onClick={() => handleShowCountry(index)}>{countryDetailsShown[index] ? 'hide' : 'show'}</button>
+                                {countryDetailsShown[index] ? <CountryDetails country={country} /> : ''}
+                            </div>
                         )
                     })
                 }
@@ -28,16 +52,7 @@ const CountryList = ({ countries, countryFilter, countriesLoaded }) => {
     } else if (filtered_countries.length === 1) {
         let single_country = filtered_countries[0];
         return (
-            <>
-                <h1>{single_country.name.common}</h1>
-                <div>capital {single_country.capital}</div>
-                <div>area {single_country.area}</div>
-                <h3>languages:</h3>
-                <ul>
-                    {Object.values(single_country.languages).map((lang, i) => <li key={i}>{lang}</li>)}
-                </ul>
-                <img src={single_country.flags.png} width={180} alt='flag' />
-            </>
+            <CountryDetails country={single_country} />
         )
     }
 }
@@ -46,6 +61,13 @@ const App = () => {
     const [countries, setCountries] = useState([])
     const [countryFilter, setCountryFilter] = useState('')
     const [countriesLoaded, setCountriesLoaded] = useState(false)
+    const [countryDetailsShown, setCountryDetailsShown] = useState([])
+
+    // dev - state change monitors
+    useEffect(() => {
+        console.log('countryDetailsShown', countryDetailsShown)
+    }, [countryDetailsShown])
+    // dev - state change monitors
 
     useEffect(() => {
         axios.get('https://restcountries.com/v3.1/all').then(resp => {
@@ -54,14 +76,22 @@ const App = () => {
         })
     }, [])
 
-    const handleFilterChange = (event) => {
-        setCountryFilter(event.target.value)
+    const handleFilterChange = (e) => {
+        setCountryFilter(e.target.value)
+
+        let filtered_countries = countries.filter(country => country.name.common.toLowerCase().includes(e.target.value.toLowerCase()));
+        setCountryDetailsShown(Array(filtered_countries.length).fill(false))
     }
 
     return (
         <>
             <label>find countries <input value={countryFilter} onChange={handleFilterChange} autoFocus /></label>
-            <CountryList countries={countries} countryFilter={countryFilter} countriesLoaded={countriesLoaded} />
+            <CountryList
+                countries={countries}
+                countryFilter={countryFilter}
+                countriesLoaded={countriesLoaded}
+                countryDetailsShown={countryDetailsShown}
+                setCountryDetailsShown={setCountryDetailsShown} />
         </>
     )
 }
