01cf6b7b4a028066bbde89303e133b792002fc58
IA21 <18244928+IA21@users.noreply.github.com>
2022-07-24 00:02:17 +0500

    3.5

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -44,6 +44,23 @@ app.get('/api/persons/:id', (req, res) => {
         : res.status(404).end()
 })
 
+app.post('/api/persons', (req, res) => {
+    let new_id, new_person = req.body;
+
+    if (!new_person.name)
+        res.status(400).json({ error: 'name required' })
+    else if (!new_person.number)
+        res.status(400).json({ error: 'number required' })
+
+    do {
+        new_id = Math.floor(Math.random() * 1000000)
+    } while (persons.map(p => p.id).find(id => id === new_id))  // keep generating ids until you get a unique one
+
+    new_person = { ...new_person, id: new_id }
+    persons.push(new_person)
+    res.json(new_person)
+})
+
 app.delete('/api/persons/:id', (req, res) => {
     persons = persons.filter(person => person.id != req.params.id)
     res.status(204).end()
