0ac53bd72c2de9ecaa8b9a1806835ac3dffb3130
IA21 <18244928+IA21@users.noreply.github.com>
2022-08-01 13:23:34 +0500

    3.14

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -49,27 +49,19 @@ app.get('/api/persons/:id', (req, res) => {
 })
 
 app.post('/api/persons', (req, res) => {
-    let new_id, new_person = req.body;
+    let new_person = req.body;
 
-    // error handling
     if (!new_person.name) {
         res.status(400).json({ error: 'name required' })
         return
     } else if (!new_person.number) {
         res.status(400).json({ error: 'number required' })
         return
-    } else if (persons.find(person => person.name.toLowerCase() === new_person.name.toLowerCase())) {
-        res.status(400).json({ error: 'name must be unique' })
-        return
     }
 
-    do {
-        new_id = Math.floor(Math.random() * 1000000)
-    } while (persons.map(p => p.id).find(id => id === new_id))  // keep generating ids until you get a unique one
-
-    new_person = { id: new_id, ...new_person }
-    persons.push(new_person)
-    res.json(new_person)
+    new Person(new_person)
+        .save()
+        .then(saved_person => res.json(saved_person))
 })
 
 app.delete('/api/persons/:id', (req, res) => {
