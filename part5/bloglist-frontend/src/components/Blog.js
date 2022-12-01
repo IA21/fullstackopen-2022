3ae48fde7d37fb5416d3297402d1956a9487786c
IA21 <18244928+IA21@users.noreply.github.com>
2022-12-01 09:54:47 +0500

    5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,7 +1,37 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+    const [expanded, setExpanded] = useState(false)
+    const BlogStyle = {
+        paddingTop: 10,
+        paddingLeft: 2,
+        border: 'solid',
+        borderWidth: 1,
+        marginBottom: 5,
+    }
+
+    const toggleExpanded = () => {
+        setExpanded(!expanded)
+    }
+
+    console.log(blog)
+
+    if (!expanded) {
+        return (
+            <div style={BlogStyle}>
+                <div>{blog.title} {blog.author} <button onClick={toggleExpanded}>view</button></div>
+            </div>
+        )
+    } else {
+        return (
+            <div style={BlogStyle}>
+                <div>{blog.title} {blog.author} <button onClick={toggleExpanded}>hide</button></div>
+                <div>{blog.url}</div>
+                <div>likes {blog.likes} <button>like</button></div>
+                <div>{blog.user.name}</div>
+            </div>
+        )
+    }
+}
 
 export default Blog
