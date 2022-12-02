70d7843f54482b71c45b3a80138da3f4edd1e9b5
IA21 <18244928+IA21@users.noreply.github.com>
2022-12-02 10:11:40 +0500

    5.11

--- part5/bloglist-frontend/src/components/Notification.js
+++ part5/bloglist-frontend/src/components/Notification.js
@@ -1,3 +1,5 @@
+import { PropTypes } from "prop-types"
+
 const Notification = ({ message, type }) => {
     if (!message)
         return <></>
@@ -7,6 +9,11 @@ const Notification = ({ message, type }) => {
                 {message}
             </div>
         )
+}
+
+Notification.propTypes = {
+    message: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
 }
 
 export default Notification
