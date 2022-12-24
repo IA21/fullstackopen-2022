8110f2ce82e571de5267d1630d887c9ccf808b9e
IA21 <18244928+IA21@users.noreply.github.com>
2022-12-24 14:20:57 +0500

    6.2

--- part6/unicafe-redux/src/index.js
+++ part6/unicafe-redux/src/index.js
@@ -6,29 +6,27 @@ import reducer from './reducer'
 const store = createStore(reducer)
 
 const App = () => {
-  const good = () => {
-    store.dispatch({
-      type: 'GOOD'
-    })
-  }
+    const good = () => store.dispatch({ type: 'GOOD' })
+    const ok = () => store.dispatch({ type: 'OK' })
+    const bad = () => store.dispatch({ type: 'BAD' })
+    const zero = () => store.dispatch({ type: 'ZERO' })
 
-  return (
-    <div>
-      <button onClick={good}>good</button>
-      <button>ok</button>
-      <button>bad</button>
-      <button>reset stats</button>
-      <div>good {store.getState().good}</div>
-      <div>ok</div>
-      <div>bad</div>
-    </div>
-  )
+    return (
+        <div>
+            <button onClick={good}>good</button>
+            <button onClick={ok}>ok</button>
+            <button onClick={bad}>bad</button>
+            <button onClick={zero}>reset stats</button>
+
+            <div>good {store.getState().good}</div>
+            <div>ok {store.getState().ok}</div>
+            <div>bad {store.getState().bad}</div>
+        </div>
+    )
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-const renderApp = () => {
-  root.render(<App />)
-}
+const renderApp = () => root.render(<App />)
 
 renderApp()
 store.subscribe(renderApp)
