c96b16d81161840a2f8dace6053387cf23879fad
IA21 <18244928+IA21@users.noreply.github.com>
2022-12-25 22:52:26 +0500

    6.5

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -27,16 +27,18 @@ const App = () => {
     return (
         <div>
             <h2>Anecdotes</h2>
-            {anecdotes.map(anecdote =>
-                <div key={anecdote.id}>
-                    <div>
-                        {anecdote.content}
+            {anecdotes
+                .sort((a, b) => b.votes - a.votes)
+                .map(anecdote =>
+                    <div key={anecdote.id}>
+                        <div>
+                            {anecdote.content}
+                        </div>
+                        <div>
+                            has {anecdote.votes} <button onClick={() => vote(anecdote.id)}>vote</button>
+                        </div>
                     </div>
-                    <div>
-                        has {anecdote.votes} <button onClick={() => vote(anecdote.id)}>vote</button>
-                    </div>
-                </div>
-            )}
+                )}
             <h2>create new</h2>
             <form onSubmit={add_anecdote}>
                 <div><input type='text' id='content' /></div>
