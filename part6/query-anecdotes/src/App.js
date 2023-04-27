aba56ef31ea69d9c2982104e9cc447a64ea61bd9
IA21 <18244928+IA21@users.noreply.github.com>
2023-04-27 10:47:16 +0500

    6.22

--- part6/query-anecdotes/src/App.js
+++ part6/query-anecdotes/src/App.js
@@ -1,9 +1,22 @@
-import { useQuery } from 'react-query'
+import { useQuery, useMutation, useQueryClient } from 'react-query'
 import axios from 'axios'
 import AnecdoteForm from './components/AnecdoteForm'
 import Notification from './components/Notification'
 
 const App = () => {
+    const queryClient = useQueryClient()
+
+    const voteMutator = useMutation({
+        mutationFn: (anecdote) => {
+            return axios
+                .put(`http://localhost:3001/anecdotes/${anecdote.id}`, anecdote)
+                .then(res => res.data)
+        },
+        onSuccess: (anecdote) => {
+            queryClient.invalidateQueries('anecdotes')
+        }
+    })
+
     const result = useQuery(
         'anecdotes',
         () => axios.get(`http://localhost:3001/anecdotes`).then(res => res.data),
@@ -23,7 +36,7 @@ const App = () => {
     const anecdotes = result.data
 
     const handleVote = (anecdote) => {
-        console.log('vote')
+        voteMutator.mutate({ ...anecdote, votes: anecdote.votes + 1 })
     }
 
     return (
